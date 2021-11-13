59b29ae5c857cd82ad79891ad97396d42feb1812
Nate Sheridan <83609521+natesheridan@users.noreply.github.com>
2021-11-12 17:40:50 -0700

    functional ticker container

--- src/components/TickerArticle/TickerArticle.js
+++ src/components/TickerArticle/TickerArticle.js
@@ -1,53 +1,55 @@
 import React, {useEffect, useState} from 'react'
+import './TickerArticle.css'
 import { getStockQuote } from './../../util/api'
 
 
 
 const TickerArticle = ({ticker}) => {
-    const [quote, setQuote] = useState(ticker);
-    const [stockElement, setStockElement] = useState(null)
-    useEffect(()=>{
-        
-        fetchData()
-    },[])
-    async function fetchData() {
-      let response = await getStockQuote(ticker)
-      setQuote(response)
-      setStockElement(generateStockElement(quote))
-    }
-    const generateStockElement = (dataArr) => {
-        const data = dataArr[0]
+    const [currentData, setCurrentData] = useState(null);
+    const [stockElement, setStockElement] = useState(null);
 
-        return (
-            <tr className="ticker-article">
-                <th className="tkr-name">{data.name}</th>
-                <th className="tkr-symbol">{data.symbol}</th>
-                <th className="tkr-price">{data.price}</th>
-                <th className="tkr-change">{data.change}</th>
-                <th className="tkr-change-percentage">{data.changePercentage}</th>
-                <th className="tkr-day-low">{data.dayLow}</th>
-                <th className="tkr-day-high">{data.dayHigh}</th>
-                <th className="tkr-open">{data.open}</th>
-            </tr>
-        )
+    useEffect(()=>{
+        fetchData()
+    }, [])
+
+    const fetchData = async () => {
+      let response = await getStockQuote(ticker)
+      setCurrentData(response[0])
+    //   let element = generateStockElement(currentData)
+    //   setStockElement(element)
     }
+
+    // const generateStockElement = (dataArr) => {
+    //     console.log(dataArr)
+    //     const data = dataArr
+
+    //     return (
+    //         <tr className="ticker-article">
+    //             <th className="tkr-name">{data?.name}</th>
+    //             <th className="tkr-symbol">{data?.symbol}</th>
+    //             <th className="tkr-price">{data?.price}</th>
+    //             <th className="tkr-change">{data?.change}</th>
+    //             <th className="tkr-change-percentage">{data?.changesPercentage}</th>
+    //             <th className="tkr-day-low">{data?.dayLow}</th>
+    //             <th className="tkr-day-high">{data?.dayHigh}</th>
+    //             <th className="tkr-open">{data?.open}</th>
+    //         </tr>
+    //     )
+    // }
 
     return (
-        <div>
-            {!stockElement
-            ? 
-            <tr className="ticker-article">
-                <th className="tkr-name">Loading...</th>
-                <th className="tkr-symbol">Loading...</th>
-                <th className="tkr-price">Loading...</th>
-                <th className="tkr-change">Loading...</th>
-                <th className="tkr-change-percentage">Loading...</th>
-                <th className="tkr-day-low">Loading...</th>
-                <th className="tkr-day-high">Loading...</th>
-                <th className="tkr-open">Loading...</th>
-            </tr> 
-            :stockElement}
-        </div>
+            <>
+                <tr className="ticker-article">
+                    <th className="tkr-name">{!currentData ?  "Loading... ": currentData.name}</th>
+                    <th className="tkr-symbol">{!currentData ?  "Loading... ": currentData.symbol}</th>
+                    <th className="tkr-price">{!currentData ?  "Loading... ": currentData.price}</th>
+                    <th className="tkr-change">{!currentData ?  "Loading... ": currentData.change}</th>
+                    <th className="tkr-change-percentage">{!currentData ?  "Loading... ": currentData.changesPercentage}</th>
+                    <th className="tkr-day-low">{!currentData ?  "Loading... ": currentData.dayLow}</th>
+                    <th className="tkr-day-high">{!currentData ?  "Loading... ": currentData.dayHigh}</th>
+                    <th className="tkr-open">{!currentData ?  "Loading... ": currentData.open}</th>
+                </tr> 
+            </>
     )
 }
 
--- src/components/TickerContainer/TickerContainer.js
+++ src/components/TickerContainer/TickerContainer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import TickerArticle from './../TickerArticle/TickerArticle'
+import './TickerContainer.css'
 
 const TickerContainer = (data, type) => {
     //type changes how the api call will be made for each article
@@ -13,6 +14,17 @@ const TickerContainer = (data, type) => {
     }
     return (
         <table className="ticker-container">
+
+            <tr className="ticker-article">
+                <th className="tkrH-name">Name</th>
+                <th className="tkrH-symbol">symbol</th>
+                <th className="tkrH-price">price</th>
+                <th className="tkrH-change">change</th>
+                <th className="tkrH-change-percentage">changePercentage</th>
+                <th className="tkrH-day-low">dayLow</th>
+                <th className="tkrH-day-high">dayHigh</th>
+                <th className="tkrH-open">open</th>
+            </tr>
             <TickerArticle ticker="AAPL"/>
         </table>
     )
