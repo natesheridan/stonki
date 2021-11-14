e6c183fa9c627f951a96e7b8af8577957b4d4afb
Nate Sheridan <83609521+natesheridan@users.noreply.github.com>
2021-11-14 01:15:19 -0700

    functional display of chart with data- adjust time displayed

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -2,26 +2,51 @@ import React, {useEffect, useState} from 'react'
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 import {getStockPriceHistory} from '../../util/api'
 
-const Chart = ({symbol, start, end, limit, type}) => {
+const Chart = ({symbol, start, end, timeseries}) => {
   const [historyData, setHistoryData] = useState(null);
+  const [params, setParams] = useState({
+    from: start,
+    to: end,
+    timeseries: timeseries
+  })
 
   useEffect(()=>{
       fetchHistory()
-  }, [])
+  }, [params])
 
   const fetchHistory = async () => {
-    let response = await getStockPriceHistory(symbol, start, end, limit, type)
-    console.log(response)
-    setHistoryData(response.historical.reverse())
+    let response = await getStockPriceHistory(symbol, params)
+    let data = response.historical
+    let count=0;
+    setHistoryData(data.reverse())
   }
-  return (  
-    <LineChart width={500} height={500} data={historyData}>
-      <Line type="monotone" dataKey="close" stroke="#00a7e1" />
-      <CartesianGrid stroke="#ccc" />
-      <XAxis dataKey="date" />
-      <YAxis dataKey='close'/>
-      <Tooltip/>
-    </LineChart>
+
+  const changeChartParams = (adjustment) => {
+    let parameters = {
+      from: start,
+      to: end,
+      timeseries: adjustment
+    }
+    setParams(parameters)
+  }
+  return (
+    <div className="chart">
+
+      <LineChart width={650} height={500} data={historyData}>
+        <Line type="monotone" dataKey="close" stroke="#00a7e1" />
+        <CartesianGrid stroke="#373737" />
+        <XAxis dataKey="date" />
+        <YAxis dataKey='close'/>
+        <Tooltip/>
+      </LineChart>
+      <section className="chart-time-buttons">
+        <button onClick={()=> {changeChartParams(5)}}>5 days</button>
+        <button onClick={()=> {changeChartParams(30)}}>30 days</button>
+        <button onClick={()=> {changeChartParams(90)}}>3 months</button>
+        <button onClick={()=> {changeChartParams(365)}}>1 year</button>
+        <button onClick={()=> {changeChartParams('alltime')}}>All time</button>
+      </section>
+    </div> 
   )
 }
 
