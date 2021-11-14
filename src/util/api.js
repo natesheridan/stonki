d2d7f953dd16259ccf84f6bfe75ab36b136995f4
Nate Sheridan <83609521+natesheridan@users.noreply.github.com>
2021-11-13 20:40:02 -0700

    Add api call for grabbing historical data

--- src/util/api.js
+++ src/util/api.js
@@ -1,10 +1,13 @@
 
 const fmp = require('financialmodelingprep')('9626b292f3ee8a5b79cb7f5e4746c12b')
 
-export const getStockQuote = (ticker) => {
-    return fmp.stock(ticker).quote().then(response => response);
+export const getStockQuote = (symbol) => {
+    return fmp.stock(symbol).quote()
     }
 export const getUserData = (userID) => {
     return fetch(`https://stonkiusers.s3.us-west-1.amazonaws.com/users.json`)
     .then (data => data)
+}
+export const getStockPriceHistory = (symbol, start, end, limit, type) => {
+    return fmp.stock(symbol).history({start, end, limit, type} = {})
 }
