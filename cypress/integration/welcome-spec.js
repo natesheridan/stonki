f174e217cd4fe4d3821cfa26cdcf51cc26869794
Nate Sheridan <83609521+natesheridan@users.noreply.github.com>
2021-12-09 22:40:15 -0700

    Revert "fixed Search feature"

--- cypress/integration/welcome-spec.js
+++ cypress/integration/welcome-spec.js
@@ -2,25 +2,25 @@ describe("homepage", () => {
     beforeEach(() => {
         cy.visit("http://localhost:3000")
     })
-
+  
     it("User should see their name visible", () => {
         cy.get('.welcome-title').first()
-            .should("contain", "John Doe")
+        .should("contain", "John Doe")
     })
     it("User should see a table of with a featured stocks title", () => {
         cy.get('.table-wrapper').first()
-            .should("contain", "Featured Stocks");
+        .should("contain", "Featured Stocks");
     })
     it("Both tables on welcome view should have a total length of 23 rows", () => {
         cy.get('tr')
-            .should("have.length", "19");
+        .should("have.length", "19");
     })
     it("First stock in table should be GOOG, and user should click symbol and see single page", () => {
         cy.get('.t-symbol').first()
-            .click();
+        .click();
         cy.get('h2').first()
-            .should("contain", "Alphabet")
+        .should("contain", "Alphabet")
     })
-
-
-})
+    
+    
+  })
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -5,12 +5,11 @@ import './Search.css'
 
 const Search = ({ amount = 3 }) => {
     const [value, setValue] = useState("");
-    const [resultTable, setResultTable] = useState([])
+    const [resultTable, setResultTable] = useState(null)
 
 
     useEffect(() => {
         fetchQueryResults(value)
-        console.log(value)
     }, [value]);
 
     const fetchQueryResults = async () => {
@@ -31,8 +30,8 @@ const Search = ({ amount = 3 }) => {
     return (
         <div className="search-view">
             <div>Search:</div>
-            <input className="search-input" type="text" placeholder="ticker / company name" value={value} onChange={onChange} />
-            <StockContainer key={resultTable.length} data={resultTable}/>
+            <input className="search-input" value={value} onChange={onChange} />
+            {value && (<>{generateContainer(resultTable)}</>)}
         </div>
     );
 }
